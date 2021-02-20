556ab830296774796a6eaed8b904f55e2de365db
Aman Tewolde <aman.tewolde@ucdenver.edu>
2021-02-20 16:55:51 -0700

    second commit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 function factorize(n){
-    var factor= [];
-    for(var i=0; i <=n; i++){
-        if(n % i== 0){
+    var factors= [];
+    for(var i = 0; i <=n; i++){
+        if(n % i == 0){
             factors.push(i);
         }
     }
-    return factores;
+    return factors;
+
+    
 }
